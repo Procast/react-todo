d164aef2efddf3ef980b4b4bc7086a00904f301b
Prashant kumar <prkunar28@gmail.com>
2022-03-14 14:13:46 +0530

    completed app

---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Todo from "./Todo";
+
+function TodoList({ todos, setTodos ,filteredTodos}) {
+  console.log(todos);
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo
+            todos={todos}
+            setTodos={setTodos}
+            todo={todo}
+            key={todo.id}
+            text={todo.text}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
